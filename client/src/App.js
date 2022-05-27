18c81266d90633b57f0cfcc3506e5168c39a5c1c
Sally <sallywing123@gmail.com>
2022-05-26 20:32:52 -0700

    Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import { Provider } from 'react-redux';
+import store from './utils/store';
+
+import Landing from './pages/Landing';
+import DisplayResult from './pages/DisplayResult';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import SavedPlaylists from './pages/SavedPlaylists'
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Provider store={store}>
+            <Nav />
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Landing />} 
+              />
+              <Route 
+                path="/signin" 
+                element={<SignIn />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<SignUp />} 
+              />
+                <Route 
+                  path="/results" 
+                  element={<DisplayResult />} 
+                />
+              <Route 
+                path="/savedplaylists" 
+                element={<SavedPlaylists />} 
+              />
+            </Routes>
+          </Provider>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
