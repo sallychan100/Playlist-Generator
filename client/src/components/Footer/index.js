bc4e9dc3c58952eea3ebe489c11159e16c08e2ba
Sally <sallywing123@gmail.com>
2022-06-01 21:51:32 -0700

    small css changes

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="w-100 mt-auto bg-secondary  background-image: linear-gradient(to right, red , yellow);
     p-4">
       <div className="container">
-        Created by TCP &copy;{new Date().getFullYear()}
+        Created by Chris, Justin & Sally &copy;{new Date().getFullYear()}
       </div>
     </footer>
   );
